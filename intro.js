039ebb2ec672d2b8a430fa9fc66d7de487878e06
XBigDeeX <osahonodavidson2000@gmail.com>
2022-06-24 12:13:12 +0100

    Assignment Completed

---
+++ intro.js
@@ -0,0 +1,4 @@
+let myName="Osahon Davidson"
+let myHeight="185cm"
+let myCountry="Nigeria"
+document.write("My name is: "+ myName + "<br>" +"My height is: " + myHeight + "<br>" + "My country of origin is: " + myCountry)
